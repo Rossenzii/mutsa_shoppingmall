1b6c384197503664f3f0a84fe4adc54c1c524cac
Rossenzii <jangmj80@naver.com>
2025-07-15 18:53:21 +0900

    [Feat] github workflow + init cors

---
+++ src/main/java/mutsa/shoppingmall/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package mutsa.shoppingmall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "http://223.130.155.172", "http://shmall.store", "https://shmall.store", "https://shmall.store:8080")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);  // 쿠키 허용
+            }
+        };
+    }
+}
