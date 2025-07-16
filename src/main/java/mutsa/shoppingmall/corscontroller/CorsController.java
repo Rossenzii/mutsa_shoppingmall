2e300480568aa802514105cef38326305bc8d4e0
Rossenzii <jangmj80@naver.com>
2025-07-16 21:41:57 +0900

    [fix] cicd

--- src/main/java/mutsa/shoppingmall/corscontroller/CorsController.java
+++ src/main/java/mutsa/shoppingmall/corscontroller/CorsController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-// cors 설정
+
 @RestController
 public class CorsController {
-
+    // cors 설정
     @RequestMapping(value = "/**", method = RequestMethod.OPTIONS)
     public ResponseEntity<Void> handleOptions() {
         return ResponseEntity.ok().build();
